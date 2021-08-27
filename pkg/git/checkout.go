be39564ed1afec00b58010d3b823988b78f60cf6
Jacek Wysocki <jacek.wysocki@kinguin.io>
2021-08-27 15:06:04 +0200

    testing git partial checkout

--- pkg/git/checkout.go
+++ pkg/git/checkout.go
@@ -1,3 +1,34 @@
-func Checkout(repo string) {
+package git
 
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/kubeshop/kubtest/pkg/process"
+)
+
+// TODO consider checkout by some go-git library to limit external deps in docker container
+// Partial checkout will checkout only given directory from Git repository
+func PartialCheckout(repo, dir string) (outputDir string, err error) {
+
+	tmpDir, err := ioutil.TempDir("", "kubtest-scripts")
+	if err != nil {
+		return tmpDir, err
+	}
+
+	fmt.Printf("%+v\n", tmpDir)
+
+	out, err := process.ExecuteInDir(tmpDir, "git",
+		"clone",
+		"--depth", "1",
+		"--filter", "blob:none",
+		"--sparse",
+		repo, "repo")
+
+	fmt.Printf("%s, %s\n", out, err)
+
+	out, err = process.ExecuteInDir(tmpDir+"/repo", "git", "sparse-checkout", "set", dir)
+	fmt.Printf("%s, %s\n", out, err)
+
+	return tmpDir + "/repo/" + dir, nil
 }
