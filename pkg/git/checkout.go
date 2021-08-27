fc5524f5ada14c066361d6d23ee1c1f41f48aedd
Jacek Wysocki <jacek.wysocki@kinguin.io>
2021-08-27 15:07:28 +0200

    Revert "simple partial checkout from Git"
    This reverts commit aefa218e7f6a8d7a7151d9cbf132ad62655587b1.

--- pkg/git/checkout.go
+++ pkg/git/checkout.go
@@ -1,31 +1,3 @@
-package git
+func Checkout(repo string) {
 
-import (
-	"fmt"
-	"io/ioutil"
-
-	"github.com/kubeshop/kubtest/pkg/process"
-)
-
-// Partial checkout will checkout only given directory from Git repository
-func PartialCheckout(repo, dir string) error {
-
-	tmpDir, err := ioutil.TempDir("", "kubtest-scripts")
-	if err != nil {
-		return err
-	}
-
-	out, err := process.Execute("git",
-		"clone",
-		"--depth", "1",
-		"--filter", "blob:none",
-		"--sparse",
-		repo, tmpDir)
-
-	fmt.Printf("%s, %s\n", out, err)
-
-	out, err = process.Execute("git", "sparse-checkout", "set", dir)
-	fmt.Printf("%s, %s\n", out, err)
-
-	return nil
 }
