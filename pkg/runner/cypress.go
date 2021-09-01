ced500bdf2864b504d89d9a3bbae7a79cfb6f07f
Jacek Wysocki <jacek.wysocki@kinguin.io>
2021-09-01 12:28:59 +0200

    moved repo validation to validotr

--- pkg/runner/cypress.go
+++ pkg/runner/cypress.go
@@ -14,15 +14,13 @@ type CypressRunner struct {
 
 func (r *CypressRunner) Run(execution kubtest.Execution) (result kubtest.ExecutionResult) {
 
+	// make come validation
+	err := r.Validate(execution)
+	if err != nil {
+		return result.Err(err)
+	}
+
 	repo := execution.Repository
-
-	if repo.Path == "" {
-		return result.Err(fmt.Errorf("can't find repository path in params, repo:%+v", repo))
-	}
-
-	if repo.Branch == "" {
-		return result.Err(fmt.Errorf("can't find branch in params, repo:%+v", repo))
-	}
 
 	// checkout repo
 	outputDir, err := git.PartialCheckout(repo.Uri, repo.Path, repo.Branch)
@@ -48,4 +46,20 @@ func (r *CypressRunner) Run(execution kubtest.Execution) (result kubtest.Executi
 		Status:    kubtest.ExecutionStatusSuceess,
 		RawOutput: string(out),
 	}
+}
+
+func (r *CypressRunner) Validate(execution kubtest.Execution) error {
+	if execution.Repository == nil {
+		return fmt.Errorf("cypress executor handle only repository based tests, but repository is nil")
+	}
+
+	if execution.Repository.Path == "" {
+		return fmt.Errorf("can't find repository path in params, repo:%+v", execution.Repository)
+	}
+
+	if execution.Repository.Branch == "" {
+		return fmt.Errorf("can't find branch in params, repo:%+v", execution.Repository)
+	}
+
+	return nil
 }
