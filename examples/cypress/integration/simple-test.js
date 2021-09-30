eed96619f37d849ae38775a8e6bca0686c6acbc2
Jacek Wysocki <jacek.wysocki@gmail.com>
2021-09-30 08:02:47 +0200

    added envs to cypress run

--- examples/cypress/integration/simple-test.js
+++ examples/cypress/integration/simple-test.js
@@ -1,6 +1,8 @@
 describe('The Home Page', () => {
   it('successfully loads', () => {
     cy.visit('https://kubtest.io') 
+
+    expect(Cypress.env('testparam')).to.equal('testvalue')
     cy.contains('Efficient testing of k8s applications')
   })
 })
