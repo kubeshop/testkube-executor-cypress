1bda76924882644f0a5113a1286fee05ffcb3293
Jasmin Gacic <jasmin.gacic@gmail.com>
2021-10-28 16:42:55 +0200

    rudementary file scrapper (#23)
    * rudementary file scrapper
    
    Signed-off-by: Jasmin Gacic <jasmin.gacic@gmail.com>
    
    * fetching parameters from env vars
    
    Signed-off-by: Jasmin Gacic <jasmin.gacic@gmail.com>
    
    * moved scrapeArtefacts to testkube lib
    
    Signed-off-by: Jasmin Gacic <jasmin.gacic@gmail.com>
    
    * moved scrapeArtefacts to testkube lib
    
    Signed-off-by: Jasmin Gacic <jasmin.gacic@gmail.com>

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -5,9 +5,23 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/kelseyhightower/envconfig"
 	"github.com/kubeshop/testkube-executor-cypress/pkg/runner"
 	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
+	"github.com/kubeshop/testkube/pkg/storage/minio"
 )
+
+type Params struct {
+	Endpoint        string // RUNNER_ENDPOINT
+	AccessKeyID     string // RUNNER_ACCESSKEYID
+	SecretAccessKey string // RUNNER_SECRETACCESSKEY
+	Location        string // RUNNER_LOCATION
+	Token           string // RUNNER_TOKEN
+	Ssl             bool   // RUNNER_SSL
+	ScrapperEnabled bool   // RUNNER_SCRAPPERENABLED
+}
+
+var params Params
 
 func main() {
 
@@ -18,11 +32,26 @@ func main() {
 	}
 
 	script := args[1]
-
+	err := envconfig.Process("runner", &params)
+	if err != nil {
+		fmt.Println("error processing parameters", err)
+		return
+	}
 	e := testkube.Execution{}
 	json.Unmarshal([]byte(script), &e)
 	runner := runner.NewCypressRunner()
 	result := runner.Run(e)
 	fmt.Println(result)
+	if params.ScrapperEnabled {
+		client, err := minio.NewClient(params.Endpoint, params.AccessKeyID, params.SecretAccessKey, params.Location, params.Token, params.Ssl) // create storage client
+		if err != nil {
+			fmt.Println("error occured creating minio client") // maybe we should consider the run failed since it is not able to save artefacts
+		}
+
+		err = client.ScrapeArtefacts(e.Id, "cypress/")
+		if err != nil {
+			fmt.Println("error occured while scrapping artefacts") // maybe we should consider the run failed since it is not able to save artefacts
+		}
+	}
 	fmt.Printf("$$$%s$$$", e.Id)
 }
--- go.mod
+++ go.mod
@@ -2,10 +2,8 @@ module github.com/kubeshop/testkube-executor-cypress
 
 go 1.16
 
-// replace github.com/kubeshop/testkube v0.5.59
-
 require (
 	github.com/joshdk/go-junit v0.0.0-20210226021600-6145f504ca0d
 	github.com/kelseyhightower/envconfig v1.4.0
-	github.com/kubeshop/testkube v0.6.1
+	github.com/kubeshop/testkube v0.6.4-0.20211028122651-83eb735dbf4a // indirect
 )
