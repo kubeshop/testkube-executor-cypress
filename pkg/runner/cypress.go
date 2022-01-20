dcf42529fe26c5495e6d49b37f1b7b6291621f36
Vladislav Sukhin <vsukhin@virtuososoft.com>
2022-01-20 11:58:17 +0300

    feat: add username and token for git repo (#32)
    * feat: add username and token for git repo
    
    * feat: user uri repo with username and token
    
    * fix: update makefile paths
    
    Co-authored-by: Vladislav Sukhin <vsukhin@kubeshop.io>

--- pkg/runner/cypress.go
+++ pkg/runner/cypress.go
@@ -2,6 +2,7 @@ package runner
 
 import (
 	"fmt"
+	"net/url"
 	"path/filepath"
 
 	junit "github.com/joshdk/go-junit"
@@ -20,6 +21,8 @@ type Params struct {
 	Token           string // RUNNER_TOKEN
 	Ssl             bool   // RUNNER_SSL
 	ScrapperEnabled bool   // RUNNER_SCRAPPERENABLED
+	GitUsername     string // RUNNER_GITUSERNAME
+	GitToken        string // RUNNER_GITTOKEN
 }
 
 func NewCypressRunner() *CypressRunner {
@@ -47,9 +50,19 @@ func (r *CypressRunner) Run(execution testkube.Execution) (result testkube.Execu
 	}
 
 	repo := execution.Repository
+	uri := repo.Uri
+	if r.Params.GitUsername != "" && r.Params.GitToken != "" {
+		gitURI, err := url.Parse(uri)
+		if err != nil {
+			return result, err
+		}
+
+		gitURI.User = url.UserPassword(r.Params.GitUsername, r.Params.GitToken)
+		uri = gitURI.String()
+	}
 
 	// checkout repo
-	outputDir, err := git.PartialCheckout(repo.Uri, repo.Path, repo.Branch)
+	outputDir, err := git.PartialCheckout(uri, repo.Path, repo.Branch)
 	if err != nil {
 		return result, err
 	}
