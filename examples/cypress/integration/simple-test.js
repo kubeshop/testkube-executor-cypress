c44ac54132a6c8c5e1a42927dd3b30a2cf5847cc
Jacek Wysocki <jacek.wysocki@gmail.com>
2022-02-03 09:33:48 +0100

    fix: examples

--- examples/cypress/integration/simple-test.js
+++ examples/cypress/integration/simple-test.js
@@ -1,9 +1,7 @@
 describe('The Home Page', () => {
   it('successfully loads', () => {
-    cy.visit('https://testkube.io') 
-
-    expect(Cypress.env('testparam')).to.equal('testvalue')
-
-    cy.contains('Efficient testing of k8s applications')
+    cy.visit('https://testkube.io');
+    expect(Cypress.env('testparam')).to.equal('testvalue');
+    cy.contains('Efficient testing of k8s applications');
   })
 })
